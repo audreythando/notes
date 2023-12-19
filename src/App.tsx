d6861405cca22cef6498986823127ccbbd3fc9aa
AudreyMpalume <129782349+AudreyMpalume@users.noreply.github.com>
2023-12-19 06:03:25 +0200

    feat/add ui elements and css

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <h1>hello</h1>
+    <div className="app-container">
+    <form className="note-form">
+      <input placeholder="Title" required />
+      <textarea placeholder="Content" rows={10} required />
+
+      <button type="submit">Add Note</button>
+    </form>
+    <div className="notes-grid">
+      <div className="note-item">
+        <div className="notes-header">
+          <button>x</button>
+        </div>
+        <h2>Note Title</h2>
+        <p>Note content</p>
+      </div>
     </div>
+  </div>
   );
 }
 
